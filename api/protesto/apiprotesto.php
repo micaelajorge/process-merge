5e7161e5c976f9d9bdcec3f3987674c1b19c68a8
micaelajorge <58042227+micaelajorge@users.noreply.github.com>
2019-12-23 19:41:58 -0200

    Adição Swagger

--- api/protesto/apiprotesto.php
+++ api/protesto/apiprotesto.php
@@ -1,37 +1,13 @@
 <?php
 
 use OpenApi\Annotations as OA;
+use raelgc\view\Template;
 
-/**
-  @OA\Info(title="Search API", version="1.0.0")
- */
+$t_body = new Template(FILES_ROOT . "assets/templates/t_swagger.html");
+$t_body->block("BLOCK_CONTENT");
 
-/**
-   * @OA\Post(
-   *     path="/search",
-   *     summary="Returns most accurate search result object",
-   *     description="Search for an object, if found return it!",
-   *     @OA\RequestBody(
-   *         description="Client side search object",
-   *         required=true,
-   *         @OA\MediaType(
-   *             mediaType="application/json",                 
-   *         @OA\Schema(ref="#/components/schemas/SearchObject")
-   *         )
-   *     ),
-   *     @OA\Response(
-   *         response=200,
-   *         description="Success",
-   *     @OA\Schema(ref="#/components/schemas/SearchResultObject)   
-   *     ), 
-   *     @OA\Response(
-   *         response=404,
-   *         description="Could Not Find Resource"
-   *     )   
-   * )
-   */
-function CarregaRetorno()
-{
-    $jDados = "teste";
-    echo $jDados;
-}
+$t_body->show();
+
+    function enviaProtesto()
+    {
+    }

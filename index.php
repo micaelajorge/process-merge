6e73bcc78e5a3247e76527b657a171595071b5dc
Marcelo <mmoscz@gmail.com>
2019-12-24 15:41:14 -0300

    - Varias implementações

--- index.php
+++ index.php
@@ -21,7 +21,7 @@ function iniciarPhpSession()
 //ini_set('display_errors', 1);
 //ini_set('display_startup_errors', 1);
 //error_reporting(E_ALL);
-ini_set("error_reporting", E_ERROR);
+//ini_set("error_reporting", E_ERROR);
 
 
 $failRequireModules = false;
